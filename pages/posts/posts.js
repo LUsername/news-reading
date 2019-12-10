c9804ab068e3e829a5aa9a7c3db69cd5976042e5
LUsername <492254446@qq.com>
2019-12-10 11:39:29 +0800

    将业务数据分离到单独的数据文件中
    使用require加载JS模块文件

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -1,5 +1,5 @@
+var postsData = require('../../data/posts-data.js');
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -8,79 +8,15 @@ Page({
     autoplay: true,
     interval: 5000,
     duration: 500,
-    circular:true
+    circular: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    let postsContainer = [{
-        date: "Sep 18 2016",
-        title: "正是虾肥蟹壮时",
-        imgSrc: "/images/post/crab.png",
-        avatar: "/images/avatar/1.png",
-        content: "菊黄蟹正肥，品尝秋之味。徐志摩把,“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
-        reading: "112",
-        collection: "96"
-    },{
-        date: "Nov 20 2016",
-        title: "比利·林恩的中场故事",
-        imgSrc: "/images/post/bl.png",
-        avatar: "/images/avatar/1.png",
-        content: "一 “李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的中场休息》，正式更名《半场无战事》。",
-        reading: 62,
-        collection: 92
-    }];
-    this.setData({postsContainer});
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onLoad: function(options) {
+    this.setData({
+      postsContainer: postsData.postList
+    });
   }
 })
