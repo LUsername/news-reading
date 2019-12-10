88f919955215377870b4c7edae9ed3285f0471de
LUsername <492254446@qq.com>
2019-12-10 17:34:19 +0800

    使用wx.showModal 添加交互反馈

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -5,17 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-    collected:false
+    collected: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var postId = options.id;
     this.data.currentPostId = postId;
     var postData = postsData.postList[postId];
-    this.setData({...postData});
+    this.setData({ ...postData
+    });
     // var postsCollected={
     //   0:true,
     //   1:false,
@@ -23,79 +24,94 @@ Page({
     //   ...
     // }
     var postsCollected = wx.getStorageSync('postsCollected');
-    if (postsCollected){
-      if (postsCollected[postId]){
-        this.setData({ collected: postsCollected[postId] });
-      }else{
+    if (postsCollected) {
+      if (postsCollected[postId]) {
+        this.setData({
+          collected: postsCollected[postId]
+        });
+      } else {
         postsCollected[postId] = false;
         wx.setStorageSync('postsCollected', postsCollected);
       }
-    }else{
+    } else {
       postsCollected = {};
       postsCollected[postId] = false;
       wx.setStorageSync('postsCollected', postsCollected);
     }
-    
   },
 
-  onCollectionTap:function(){
+  onCollectionTap: function() {
+    // wx.showToast({
+    //   title: collected?'收藏成功':'取消成功',
+    //   duration:1000
+    // })
     var postsCollected = wx.getStorageSync('postsCollected');
     var collected = postsCollected[this.data.currentPostId];
-    collected = !collected;
-    postsCollected[this.data.currentPostId] = collected;
-    wx.setStorageSync('postsCollected', postsCollected);
-    this.setData({ collected});
-    wx.showToast({
-      title: collected?'收藏成功':'取消成功',
-      duration:1000
+    wx.showModal({
+      // title: '收藏',
+      content: collected?'是否取消收藏':'是否收藏该文章',
+      cancelText: '取消',
+      confirmText: '确认',
+      cancelColor: '#333',
+      confirmColor: '#405f80',
+      success: (res)=> {
+        if (res.confirm) {
+          collected = !collected;
+          postsCollected[this.data.currentPostId] = collected;
+          wx.setStorageSync('postsCollected', postsCollected);
+          this.setData({
+            collected
+          });
+        }
+      }
     })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
